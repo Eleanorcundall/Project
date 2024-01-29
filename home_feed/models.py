bc0494aa8da47f096553128e6087ced07d9e1625
Eleanorcundall Cundall <eleanorcundall@hotmail.com>
2024-01-29 16:34:37 +0000

    Install cloudinary and Django autoSlug, Create first main blog post model and make migrations

--- home_feed/models.py
+++ home_feed/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from autoslug import AutoSlugField 
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class BlogPost(models.Model):
+    CATEGORY_CHOICES = [
+        ('Hair', 'Hair'),
+        ('Skin', 'Skin'),
+        ('Sexual Wellness', 'Sexual Wellness'),
+        ('Menstrual Wellness', 'Menstrual Wellness'),
+        
+    ]
+
+    STATUS_CHOICES = [
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+        ('archived', 'Archived'),
+    ]
+
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    slug = AutoSlugField(populate_from='title', unique=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='draft')
+
+    def __str__(self):
+        return self.title
--- itgirl/settings.py
+++ itgirl/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'home_feed',
 ]
 
@@ -90,6 +92,10 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
