93f3ddb845af3feaea6f51126ea8a7fdf010c1db
Eleanorcundall Cundall <eleanorcundall@hotmail.com>
2024-01-30 20:55:35 +0000

    Install Faker and set up seed data for development

---
+++ home_feed/management/commands/seed_data.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from home_feed.models import BlogPost
+from faker import Faker
+
+fake = Faker()
+
+class Command(BaseCommand):
+    help = 'Seed the database with dummy data'
+
+    def handle(self, *args, **options):
+        self.stdout.write(self.style.SUCCESS('Seeding data...'))
+
+        for _ in range(90):
+            title = fake.sentence()
+            content = fake.paragraph()
+            category = fake.random_element(elements=('Hair', 'Skin', 'Sexual Wellness', 'Menstrual Wellness'))
+            author = User.objects.first()
+            BlogPost.objects.create(title=title, content=content, category=category, author=author, status='published')
+
+
+        self.stdout.write(self.style.SUCCESS('Data seeding complete.'))
