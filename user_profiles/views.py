81134175bb22b2c7eef03296bfdb5837c5aea333
Eleanorcundall Cundall <eleanorcundall@hotmail.com>
2024-02-08 17:54:06 +0000

    Amend view for user_profile to populate forms with existing instance of the logged in users profile data

--- user_profiles/views.py
+++ user_profiles/views.py
@@ -3,14 +3,19 @@ from .models import UserProfile
 from .forms import UserProfileForm
 
 def submit_user_profile_form(request):
+    try:
+        user_profile = UserProfile.objects.get(user=request.user)
+    except UserProfile.DoesNotExist:
+        user_profile = None
+
     if request.method == 'POST':
-        form = UserProfileForm(request.POST, request.FILES)
+        form = UserProfileForm(request.POST, request.FILES, instance=user_profile)
         if form.is_valid():
             user_profile = form.save(commit=False)
             user_profile.user = request.user 
             user_profile.save()
             return redirect('home')
     else:
-        form = UserProfileForm()
+        form = UserProfileForm(instance=user_profile)
 
     return render(request, 'user_profiles/user_profile_edit.html', {'form': form})
