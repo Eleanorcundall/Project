457e3d20c9bd7425ff5982742f91e2d870faf83f
Eleanorcundall Cundall <eleanorcundall@hotmail.com>
2024-02-08 10:48:51 +0000

    Create UserProfileForm for users to update their profiles

---
+++ user_profiles/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import UserProfile
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['user', 'bio', 'favorite_products', 'profile_picture']
+        exclude = ['likes_received', 'likes_given']
