fdb999c86385ae8ed3e03361857b1b27af0eb0b9
Eleanorcundall Cundall <eleanorcundall@hotmail.com>
2024-02-16 12:33:07 +0000

    Develop the like functionality by Creating a like_post view that enables the user to only like an instance on a post once

--- content_feeds/views.py
+++ content_feeds/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import BlogPost
+from .models import BlogPost, Like
 from user_submissions.models import UserSubmission
 
 
@@ -38,3 +38,17 @@ def category_view(request, category):
 
     all_posts.sort(key=lambda x: x.created_at, reverse=True)
     return render(request, 'content_feeds/category_posts.html', {'all_posts': all_posts, 'category': category})
+
+
+@login_required
+def like_post(request, post_id):
+    post = get_object_or_404(UserSubmission, id=post_id)
+    user = request.user
+
+    if Like.objects.filter(user=user, content_type__model='usersubmission', object_id=post_id).exists():
+        return JsonResponse({'error': 'You have already liked this post'}, status=400)
+    else:
+        like = Like(user=user, content_type=post.get_content_type(), object_id=post_id)
+        like.save()
+        print("post liked!")
+        return JsonResponse({'success': 'Post liked successfully'})
