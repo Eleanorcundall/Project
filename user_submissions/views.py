c38663f3e60a536a215277c54f41e670b3df0014
Eleanor Cundall <eleanorcundall@hotmail.com>
2024-10-21 17:34:34 +0100

    Create view for user to delete their own submissions/posts

--- user_submissions/views.py
+++ user_submissions/views.py
@@ -1,7 +1,11 @@
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from .models import UserSubmission
 from .forms import UserSubmissionForm
 
+@login_required
 def submit_post(request):
     print(request.method)
     if request.method == 'POST':
@@ -15,3 +19,18 @@ def submit_post(request):
         user_submission_form = UserSubmissionForm()
 
     return render(request, 'user_submissions/submit_post.html', {'user_submission_form': user_submission_form})
+
+@login_required
+def delete_post(request, slug):
+    post = get_object_or_404(UserSubmission, slug=slug)
+
+    if post.user != request.user:
+        messages.error(request, "You are not authorized to delete this post.")
+        return redirect('blog_post_detail', slug=slug)
+
+    if request.method == "POST":
+        post.delete()
+        messages.success(request, "Post deleted successfully.")
+        return redirect('category_view', category=post.category)
+    
+    return render(request, 'content_feeds/delete_post_confirm.html', {'post': post})
