3b624a1c9a82267114f7d8ee10bdeb45b5a0c287
Eleanor Cundall <eleanorcundall@hotmail.com>
2024-10-30 18:40:23 +0000

    Install and set up tailwind css, replace bootstrap tag and add test class

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/base.html',
+    './static/src/**/*.js',
+    './content_feeds/templates/**/*.html',
+    './user_profile/templates/**/*.html',
+    './user_submissions/templates/**/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
